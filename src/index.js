a504b877712173a0d3ca7c5cc64c3483b09e1b61
Stephen <stephenbrady365@gmail.com>
2021-04-15 10:22:01 +0100

    Add function to create entry card & add to dom

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ const DB_NAME = "indexedDBDemo";
 const initializeDBBtn = document.querySelector(".initialize-db-btn");
 const deleteDBBtn = document.querySelector(".delete-db-btn");
 const form = document.querySelector("form");
+const entriesSection = document.getElementsByClassName("db-entries")[0];
 let dbExists = false;
 let db;
+let store;
 
 (async function () {
   const dbs = await indexedDB.databases();
@@ -27,6 +29,8 @@ initializeDBBtn.addEventListener("click", () => {
 
   DBOpenRequest.onsuccess = function () {
     db = DBOpenRequest.result;
+    store = db.createObjectStore("userStore");
+    store.createIndex("user", "user");
     dbExists = true;
   };
 });
@@ -44,5 +48,78 @@ form.addEventListener("submit", (event) => {
 });
 
 form.addEventListener("formdata", (event) => {
-  for (const val of event.formData.keys()) console.log(val);
+  //extract form data into object for ease of use
+  const fd = event.formData;
+  const data = {};
+  for (const key of fd.keys()) {
+    if (key === "languages") data[key] = fd.getAll(key);
+    else data[key] = fd.get(key);
+  }
+
+  // addDataToDB(data);
+  addEntryCardToDom(data);
 });
+
+function addDataToDB(data) {
+  const transaction = db.transaction("userStore", "readwrite");
+  const userStore = transaction.objectStore("userStore");
+  userStore.add(data);
+}
+
+function addEntryCardToDom(data) {
+  const article = document.createElement("article");
+  const id = makeId();
+  article.setAttribute("id", id);
+  article.setAttribute("class", "entry-card");
+
+  const dl = document.createElement("dl");
+  dl.setAttribute("class", "entry-detail-list");
+
+  for (const key in data) {
+    const div = document.createElement("div");
+    const dt = document.createElement("dt");
+    const dd = document.createElement("dd");
+
+    const keyName = document.createTextNode(key);
+    dt.appendChild(keyName);
+
+    const value = document.createTextNode(data[key]);
+    dd.appendChild(value);
+
+    div.appendChild(dt);
+    div.appendChild(dd);
+
+    dl.appendChild(div);
+  }
+
+  article.appendChild(dl);
+
+  const divForBtn = document.createElement("div");
+  const deleteBtn = document.createElement("button");
+
+  deleteBtn.setAttribute("type", "button");
+  deleteBtn.setAttribute("class", "delete-entry-btn");
+  deleteBtn.setAttribute("id", `${id}-btn`);
+  const deleteText = document.createTextNode("Delete");
+  deleteBtn.appendChild(deleteText);
+  deleteBtn.addEventListener("click", () => {
+    deleteBtn.parentNode.parentNode.parentNode.removeChild(id);
+  });
+
+  divForBtn.appendChild(deleteBtn);
+  article.appendChild(divForBtn);
+  console.log(entriesSection);
+
+  entriesSection.appendChild(article);
+}
+
+function makeId() {
+  const result = [];
+  const chars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charsLength = chars.length;
+  for (let i = 0; i < 5; i++) {
+    result.push(chars.charAt(Math.floor(Math.random() * charsLength)));
+  }
+  return result.join("");
+}
